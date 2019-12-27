f9c1f6c8bd7b23fdfa93de8d4231b9545c9d5060
Kad Venku <venkukad@gmail.com>
2019-12-27 15:34:05 +0100

    chore(Migration Unit): Added Translation xsd

---
+++ eaw-build/data/config/text/v1/LocalisationDataType.cs
@@ -0,0 +1,31 @@
+﻿namespace eaw.build.data.config.text.v1
+{
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.8.3928.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(Namespace="http://www.example.org/eaw-translation/")]
+    [System.Xml.Serialization.XmlRootAttribute("LocalisationData", Namespace="http://www.example.org/eaw-translation/", IsNullable=false)]
+    public class LocalisationDataType : object, System.ComponentModel.INotifyPropertyChanged {
+    
+        private LocalisationType[] _localisationField;
+    
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("Localisation", Form=System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public LocalisationType[] Localisation {
+            get => _localisationField;
+            set {
+                _localisationField = value;
+                RaisePropertyChanged("Localisation");
+            }
+        }
+    
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+    
+        protected void RaisePropertyChanged(string propertyName) {
+            System.ComponentModel.PropertyChangedEventHandler propertyChanged = PropertyChanged;
+            propertyChanged?.Invoke(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
