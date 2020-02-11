cf12820dff6e74c6523afa22c4da86471f415ec7
KadVenku <venkukad@gmail.com>
2020-02-11 16:50:17 +0100

    chore(CodeQuality): Fix Coverage

--- .tools/travis-ci-build.sh
+++ .tools/travis-ci-build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo "Executing MSBuild DLL begin command..."
-dotnet .tools/sonar/SonarScanner.MSBuild.dll begin /o:${SONAR_ORGANIZATION} /k:${SONAR_PROJECT} /d:sonar.cs.vstest.reportsPaths="**/TestResults/*.trx" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
+dotnet .tools/sonar/SonarScanner.MSBuild.dll begin /o:${SONAR_ORGANIZATION} /k:${SONAR_PROJECT} /d:sonar.cs.vstest.reportsPaths="**/TestResults/*.trx" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN} /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*" /d:sonar.coverage.exclusions="eaw-build.test/**,**/*Tests.cs,**/TestUtility.cs" /d:sonar.cs.opencover.reportsPaths="${dir}\lcov.opencover.xml"
 echo "Running build..."
 dotnet build eaw-build.sln
 echo "Running tests..."
-dotnet test eaw-build.test/eaw-build.test.csproj --logger:trx
+dotnet test eaw-build.test/eaw-build.test.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=\"opencover,lcov\" /p:CoverletOutput=../lcov
 echo "Executing MSBuild DLL end command..."
 dotnet .tools/sonar/SonarScanner.MSBuild.dll end /d:sonar.login=${SONAR_TOKEN}
